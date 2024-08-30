d61010c06edf2c676ed99b890b90a4bc662a9f63
Faizan Sayyad <faizansayyad126@gmail.com>
2024-08-30 23:31:01 +0530

    First commit

--- app/src/main/java/com/example/firstapp/MainActivity.java
+++ app/src/main/java/com/example/firstapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.firstapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +21,6 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Toast toast = Toast.makeText(this, "Hello World!", Toast.LENGTH_SHORT);
     }
 }
